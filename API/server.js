9789412a754ecd9eb1c2e30162c55f9896b1ef04
longdan <1060921782@qq.com>
2018-09-10 16:03:13 +0800

    对聊天细节进行修饰

--- API/server.js
+++ API/server.js
@@ -1,24 +1,25 @@
 const ws = require("nodejs-websocket")
-const arr=[];
 let connectionNum = 0;
 const server = ws.createServer(function (conn) {
 	console.log("New connection")
 	connectionNum++;
-	borderCast(connectionNum+"com in")
+	conn.nickName = "用户"+connectionNum;
+	borderCast(conn.nickName+"com in","enter")
 	conn.on("text", function (str) {
 		console.log("Received "+str)
-		borderCast(str)
+		borderCast(conn.nickName+"said:" +str,"text")
 	})
 	conn.on("close", function (code, reason) {
 		console.log("Connection closed")
+		borderCast(conn.nickName+"leave","leave")
 	})
 	conn.on("error",function(err) {
 		console.log("error handle:"+err)
 	})
 }).listen(3000)
-function borderCast(str){
+function borderCast(str,type){
 	server.connections.forEach(connection=>{
-		connection.sendText(str);
+		connection.sendText(JSON.stringify({type,data:str}));
 	})
 }
 console.log("server is listening port 3000...");
--- src/js/send.js
+++ src/js/send.js
@@ -8,5 +8,17 @@ send.onclick = ()=>{
 	socket.send(document.querySelector("#sendMs").value);
 }
 socket.onmessage = (res)=>{
-	ms.innerHTML += "<br/>"+res.data;
+	let p = document.createElement("p");
+	res = JSON.parse(res.data);
+	p.innerHTML += "<br/>"+res.data;
+	if (res.type === "enter") {
+		p.style.color = "blue";
+	}
+	else if (res.type==="text") {
+		p.style.color = "black";
+	}
+	else {
+		p.style.color = "green";
+	}
+	ms.appendChild(p);
 }
